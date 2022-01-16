6423e28ffff1604e50b6b8fd1281206fd65a8359
fmeccanici <florismeccanici@mecctech-solutions.nl>
2022-01-16 19:04:37 +0100

    Add mecctech red color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
       "./resources/**/*.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+        colors : {
+            "mecctech-red" : "#e30613"
+        }
+    },
   },
   plugins: [],
 }
