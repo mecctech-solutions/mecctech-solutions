b3913c6b989fb13bdb2cd7db1bf8b34afa061cf9
fmeccanici <florismeccanici@mecctech-solutions.nl>
2022-01-27 15:58:34 +0100

    Add PortfolioItem.vue

---
+++ resources/js/components/PortfolioItem.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['description', 'title', 'svg_d'],
+        mounted() {
+
+        }
+    }
+</script>
