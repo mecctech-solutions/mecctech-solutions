2bd5cfc78567a063c0719e52b1ce5ee7ea836049
fmeccanici <florismeccanici@mecctech-solutions.nl>
2022-02-06 20:19:19 +0100

    feat: add seeder for portfolio items

---
+++ database/seeders/PortfolioItemsSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\PortfolioManagement\Domain\PortfolioItems\PortfolioItemFactory;
+use App\PortfolioManagement\Infrastructure\Persistence\Eloquent\PortfolioItems\Repositories\EloquentPortfolioItemRepository;
+use Illuminate\Database\Seeder;
+
+class PortfolioItemsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $portfolioItemRepository = new EloquentPortfolioItemRepository();
+        $portfolioItems = PortfolioItemFactory::create(10);
+        $portfolioItemRepository->addMultiple($portfolioItems);
+    }
+}
