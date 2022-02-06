7a695a28f40baac0829cbe6597fe9bcd11a5dbf8
fmeccanici <florismeccanici@mecctech-solutions.nl>
2022-02-06 19:10:35 +0100

    feat: add methods for use cases

--- app/PortfolioManagement/Presentation/Http/Api/PortfolioManagementController.php
+++ app/PortfolioManagement/Presentation/Http/Api/PortfolioManagementController.php
@@ -4,12 +4,56 @@
 namespace App\PortfolioManagement\Presentation\Http\Api;
 
 
+use App\PortfolioManagement\Application\GetAllPortfolioItems\GetAllPortfolioItems;
+use App\PortfolioManagement\Application\GetAllPortfolioItems\GetAllPortfolioItemsInput;
+use App\PortfolioManagement\Application\GetPortfolioItemsWithTag\GetPortfolioItemsWithTag;
+use App\PortfolioManagement\Application\GetPortfolioItemsWithTag\GetPortfolioItemsWithTagInput;
+use App\PortfolioManagement\Domain\Repositories\PortfolioItemRepositoryInterface;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 
 class PortfolioManagementController
 {
-    public function index(Request $request)
+    public function getAllPortfolioItems(Request $request): array
     {
-        // Handle the response
+        try {
+            $useCase = new GetAllPortfolioItems(App::make(PortfolioItemRepositoryInterface::class));
+            $useCaseInput = new GetAllPortfolioItemsInput();
+            $useCaseResult = $useCase->execute($useCaseInput);
+
+            $response["meta"]["created_at"] = time();
+            $response["payload"]["portfolio_items"] = $useCaseResult->portfolioItems();
+
+        } catch (\Exception $e)
+        {
+            $response["meta"]["created_at"] = time();
+            $response["error"]["code"] = $e->getCode();
+            $response["error"]["message"] = $e->getMessage();
+        }
+
+        return $response;
+    }
+
+    public function getPortfolioItemsWithTag(Request $request)
+    {
+        try {
+            $useCase = new GetPortfolioItemsWithTag(App::make(PortfolioItemRepositoryInterface::class));
+            $tag = $request->input("tag");
+            $useCaseInput = new GetPortfolioItemsWithTagInput([
+                "tag" => $tag
+            ]);
+            $useCaseResult = $useCase->execute($useCaseInput);
+
+            $response["meta"]["created_at"] = time();
+            $response["payload"]["portfolio_items"] = $useCaseResult->portfolioItems();
+
+        } catch (\Exception $e)
+        {
+            $response["meta"]["created_at"] = time();
+            $response["error"]["code"] = $e->getCode();
+            $response["error"]["message"] = $e->getMessage();
+        }
+
+        return $response;
     }
 }
