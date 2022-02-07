03568ba0c25b665155e1f5bae70ca5e24041fe2e
fmeccanici <florismeccanici@mecctech-solutions.nl>
2022-02-07 13:56:52 +0100

    fix: add styling and change main image url

--- resources/js/components/PortfolioItems.vue
+++ resources/js/components/PortfolioItems.vue
@@ -3,8 +3,8 @@
         <h1 class="text-4xl font-bold mt-10">PROJECTS</h1>
         <div class="border-t border-4 border-black mt-5 w-1/16"></div>
 
-        <div class="flex justify-center" v-for="portfolio_item in this.portfolio_items">
-            <portfolio-item :title="portfolio_item.title" :description="portfolio_item.description" :tags="portfolio_item.tags" :main_image_url="portfolio_item.main_image_url" :website_url="portfolio_item.website_url" :image_urls="portfolio_item.image_urls"></portfolio-item>
+        <div class="grid grid-cols-2">
+            <portfolio-item v-for="portfolio_item in this.portfolio_items" :title="portfolio_item.title" :description="portfolio_item.description" :tags="portfolio_item.tags" :main_image_url="portfolio_item.main_image.url" :website_url="portfolio_item.website_url" :image_urls="portfolio_item.image_urls"></portfolio-item>
         </div>
     </div>
 </template>
@@ -20,7 +20,7 @@
                 axios
                     .get(this.get_all_portfolio_items_route)
                     .then(( { data } ) => {
-                        this.portfolio_items = data.payload.portfolio_items
+                        this.portfolio_items = data.payload.portfolio_items;
                     });
             },
             getPortfolioItemsWithTag(tag) {
