515e2bfdd912f7620d294d8f6915196c8a5aca7e
fmeccanici <florismeccanici@mecctech-solutions.nl>
2022-02-07 19:51:11 +0100

    feat: add css

--- resources/js/components/PortfolioItems.vue
+++ resources/js/components/PortfolioItems.vue
@@ -3,7 +3,7 @@
         <h1 class="text-4xl font-bold mt-10">PROJECTS</h1>
         <div class="border-t border-4 border-black mt-5 w-1/16"></div>
 
-        <div class="grid grid-cols-2">
+        <div class="grid grid-cols-3 pl-96 pr-96 mt-20">
             <portfolio-item v-for="portfolio_item in this.portfolio_items" :title="portfolio_item.title" :description="portfolio_item.description" :tags="portfolio_item.tags" :main_image_url="portfolio_item.main_image.url" :website_url="portfolio_item.website_url" :image_urls="portfolio_item.image_urls"></portfolio-item>
         </div>
     </div>
@@ -14,6 +14,7 @@
         props: ["get_all_portfolio_items_route", "get_portfolio_items_with_tag_route", "portfolio_items"],
         mounted() {
             this.getAllPortfolioItems();
+            console.log(this.portfolio_items);
         },
         methods: {
             getAllPortfolioItems() {
