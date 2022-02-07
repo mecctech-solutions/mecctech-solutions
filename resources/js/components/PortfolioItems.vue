74ca9b341ab448b7b6ff9a459e48c0420532fa80
fmeccanici <florismeccanici@mecctech-solutions.nl>
2022-02-07 13:35:30 +0100

    fix: correct axios calls and styling

--- resources/js/components/PortfolioItems.vue
+++ resources/js/components/PortfolioItems.vue
@@ -3,7 +3,7 @@
         <h1 class="text-4xl font-bold mt-10">PROJECTS</h1>
         <div class="border-t border-4 border-black mt-5 w-1/16"></div>
 
-        <div v-for="portfolio_item in this.portfolio_items">
+        <div class="flex justify-center" v-for="portfolio_item in this.portfolio_items">
             <portfolio-item :title="portfolio_item.title" :description="portfolio_item.description" :tags="portfolio_item.tags" :main_image_url="portfolio_item.main_image_url" :website_url="portfolio_item.website_url" :image_urls="portfolio_item.image_urls"></portfolio-item>
         </div>
     </div>
@@ -13,14 +13,22 @@
     export default {
         props: ["get_all_portfolio_items_route", "get_portfolio_items_with_tag_route", "portfolio_items"],
         mounted() {
-            this.portfolio_items = this.getAllPortfolioItems();
+            this.getAllPortfolioItems();
         },
         methods: {
             getAllPortfolioItems() {
-                return axios.get(this.get_all_portfolio_items_route);
+                axios
+                    .get(this.get_all_portfolio_items_route)
+                    .then(( { data } ) => {
+                        this.portfolio_items = data.payload.portfolio_items
+                    });
             },
             getPortfolioItemsWithTag(tag) {
-                return axios.get(this.get_portfolio_items_with_tag_route);
+                axios
+                    .get(this.get_portfolio_items_with_tag_route + "/" + tag)
+                    .then(( { data } ) => {
+                        this.portfolio_items = data.payload.portfolio_items
+                    });
             }
         }
 
