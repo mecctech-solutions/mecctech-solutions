3e815a486c0ae960bdb44e5bc1d3399c67088512
fmeccanici <florismeccanici@mecctech-solutions.nl>
2022-02-07 11:30:07 +0100

    feat: make test pass

--- app/Http/Middleware/HandleCorsMiddleware.php
+++ app/Http/Middleware/HandleCorsMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class HandleCorsMiddleware
 {
@@ -17,17 +18,14 @@ class HandleCorsMiddleware
     public function handle(Request $request, Closure $next)
     {
         $headers = [
-            'Access-Control-Allow-Methods'=> 'POST, GET, OPTIONS, PUT, DELETE',
-            'Access-Control-Allow-Headers'=> 'Content-Type, X-Auth-Token, Origin'
+            'Access-Control-Allow-Origin' => "*"
         ];
-        if($request->getMethod() == "OPTIONS") {
 
-            return response()->make('OK', 200, $headers);
+        foreach ($headers as $key => $value)
+        {
+            $request->headers->set($key, $value);
         }
 
-        $response = $next($request);
-        foreach($headers as $key => $value)
-            $response->header($key, $value);
-        return $response;
+        return $next($request);
     }
 }
