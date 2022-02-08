7d77f24e157e596d16357fe4562d30a88b5ee5d9
fmeccanici <florismeccanici@mecctech-solutions.nl>
2022-02-08 19:48:25 +0100

    feat: start with modal for when clicking on learn more button

---
+++ resources/js/components/PortfolioItemModal.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="flex justify-center items-center" @mouseleave="this.$emit('turn-off-modal')">
+        <div class="bg-white z-10 absolute p-96 m-auto border-black border-2">
+            TESTTEST
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['title', 'tags', 'main_image_url', 'image_urls', 'description', 'website_url', 'view_modal'],
+        mounted() {
+        },
+        methods : {
+
+        }
+
+    }
+</script>
+
+<style>
+
+</style>
