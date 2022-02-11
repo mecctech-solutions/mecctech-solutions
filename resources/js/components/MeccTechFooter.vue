46c865118e6b416ba2b0b55eb3ca2f9347518b1f
fmeccanici <florismeccanici@mecctech-solutions.nl>
2022-02-11 22:56:51 +0100

    feat: add social icons

--- resources/js/components/MeccTechFooter.vue
+++ resources/js/components/MeccTechFooter.vue
@@ -1,12 +1,18 @@
 <template>
-    <div class="w-full bg-gray-200 text-black flex flex-col items-center">
-        <div class="flex p-10">
+    <div class="w-full bg-gray-200 text-black flex flex-col items-center justify-center h-80">
+        <div class="flex p-10 items-center">
+            <div class="grid grid-cols-3 gap-8 mb-5 mr-32">
+                <i class="fa-brands fa-linkedin text-5xl hover:text-mecctech-red transition ease-in-out duration-500 cursor-pointer"></i>
+                <i class="fa-brands fa-facebook text-5xl hover:text-mecctech-red transition ease-in-out duration-500 cursor-pointer"></i>
+                <i class="fa-brands fa-instagram text-5xl hover:text-mecctech-red transition ease-in-out duration-500 cursor-pointer"></i>
+            </div>
+
             <img class="w-32" src="images/Mecctech_Solutions.png" alt="">
             <div class="flex flex-col items-left ml-10">
-                <h1>Floris Meccanici</h1>
-                <h1>MeccTech Solutions</h1>
-                <h1>+31681639449</h1>
-                <h1>KvK: 80904246</h1>
+                <h1 class="font-bold">Floris Meccanici</h1>
+                <h1 class="font-bold">MeccTech Solutions</h1>
+                <h1 class="font-bold">+31681639449</h1>
+                <h1 class="font-bold">KvK: 80904246</h1>
             </div>
         </div>
 
