c82cc916cd758815f56c16bebae28b0b39f72961
fmeccanici <florismeccanici@mecctech-solutions.nl>
2022-02-11 16:04:14 +0100

    feat: add section title for reuse

---
+++ resources/js/components/SectionTitle.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="flex flex-col justify-center items-center">
+        <h1 class="text-4xl font-bold mt-10">{{ title }}</h1>
+        <div class="border-t border-4 border-black mt-5 w-1/16"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['title'],
+        mounted() {
+
+        },
+        methods : {
+
+        }
+    }
+</script>
