c30e18184a5f2a035ec5ffa0b1fc88719e549ad4
fmeccanici <florismeccanici@mecctech-solutions.nl>
2022-02-11 20:04:37 +0100

    feat: add footer

---
+++ resources/js/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="w-full">
+
+    </div>
+</template>
+
+<script>
+
+    export default {
+        mounted() {
+
+        },
+        methods : {
+
+        }
+    }
+</script>
