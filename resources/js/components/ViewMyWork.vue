74eee2adb3fb71a8d3a7d2baef048375840f8adc
fmeccanici <florismeccanici@mecctech-solutions.nl>
2022-02-11 20:57:56 +0100

    feat: update text for marketing reasons

--- resources/js/components/ViewMyWork.vue
+++ resources/js/components/ViewMyWork.vue
@@ -1,9 +1,11 @@
 <template>
     <div class="m-auto flex flex-col items-center">
-        <img class="pb-32" src="images/Mecctech_Solutions.png" alt="">
+        <img class="" src="images/Mecctech_Solutions.png" alt="">
+        <h1 class="text-4xl pb-32 mt-5 mr-4">E-commerce web applications.</h1>
+
         <p class="text-5xl">Hi, I'm <span class="text-mecctech-red font-bold">Floris Meccanici</span>.</p>
         <p class="text-3xl ml-10">I'm a <span class="typing"></span></p>
-        <button @click="scrollToHeader" class="btn view-my-work-btn mt-20 text-2xl">View my work</button>
+        <button @click="scrollToHeader" class="btn view-my-work-btn mt-24 text-2xl">View my work</button>
     </div>
 </template>
 
@@ -15,7 +17,7 @@
             console.log('Component mounted.');
 
             var typed = new Typed(".typing", {
-                strings: ['full-stack web developer.', 'freelancer.', 'software engineer.', 'Laravel developer.', 'PHP developer.', 'Vue.js developer.'],
+                strings: ['full-stack web developer.', 'Laravel developer.', 'Vue.js developer.', 'freelancer.', 'software engineer.'],
                 typeSpeed: 100,
                 backSpeed: 60,
                 loop: true
