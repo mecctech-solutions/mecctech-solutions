ba0c35cfebcc34ccf2afcc1c15acd39211f3d9f1
fmeccanici <florismeccanici@mecctech-solutions.nl>
2022-02-13 20:15:23 +0100

    feat: add logic for selecting header elements

--- resources/js/components/MeccTechHeader.vue
+++ resources/js/components/MeccTechHeader.vue
@@ -1,10 +1,10 @@
 <template>
-    <nav id="header">
+    <nav id="header" class="sticky">
         <ul class="flex space-x-5 bg-black text-xl text-white border-b border-b-4" style="border-color: #e30613;">
-            <li id="header-home" @click="scrollToHome(); makeAllNavElementsTextWhite(); makeNavElementTextRed('header-home')" class="p-5 hover:text-mecctech-red ease-in-out duration-500 cursor-pointer">HOME</li>
-            <li id="header-about-me" class="p-5 hover:text-mecctech-red ease-in-out duration-500 cursor-pointer">ABOUT ME</li>
-            <li class="p-5 hover:text-mecctech-red ease-in-out duration-500 cursor-pointer">PORTFOLIO</li>
-            <li class="p-5 hover:text-mecctech-red ease-in-out duration-500 cursor-pointer">CONTACT</li>
+            <li id="header-home" @click="scrollToHome(); selectNavElement('home')" class="p-5 hover:text-mecctech-red ease-in-out duration-500 cursor-pointer">HOME</li>
+            <li id="header-about-me" @click="scrollToAboutMe(); selectNavElement('about-me')" class="p-5 hover:text-mecctech-red ease-in-out duration-500 cursor-pointer">ABOUT ME</li>
+            <li id="header-portfolio" @click="scrollToPortfolio(); selectNavElement('portfolio')" class="p-5 hover:text-mecctech-red ease-in-out duration-500 cursor-pointer">PORTFOLIO</li>
+            <li id="header-contact" @click="scrollToContactForm(); selectNavElement('contact')" class="p-5 hover:text-mecctech-red ease-in-out duration-500 cursor-pointer">CONTACT</li>
         </ul>
     </nav>
 </template>
@@ -19,6 +19,34 @@
             scrollToHome() {
                 let el = document.getElementById('home');
                 el.scrollIntoView();
+            },
+            scrollToPortfolio() {
+                let el = document.getElementById('projects');
+                el.scrollIntoView();
+            },
+            scrollToContactForm() {
+                let el = document.getElementById('contact');
+                el.scrollIntoView();
+            },
+            scrollToAboutMe() {
+                let el = document.getElementById('about-me');
+                el.scrollIntoView();
+            },
+            selectNavElement(navElement) {
+                this.makeAllNavElementsTextWhite();
+                if (navElement === 'home')
+                {
+                    this.makeNavElementTextRed('header-home');
+                } else if (navElement === 'about-me')
+                {
+                    this.makeNavElementTextRed('header-about-me');
+                } else if (navElement === 'portfolio')
+                {
+                    this.makeNavElementTextRed('header-portfolio');
+                } else if (navElement === 'contact')
+                {
+                    this.makeNavElementTextRed('header-contact');
+                }
             },
             makeAllNavElementsTextWhite() {
                 document.getElementById('header-home').style.color = "#fffff";
