1fbdd070a2f6d447a84f8c382a9990cf06eb6d65
fmeccanici <florismeccanici@mecctech-solutions.nl>
2022-02-24 14:14:49 +0100

    test: add test to create portfolio item from array

--- tests/Unit/PortfolioManagement/PortfolioItemFactoryTest.php
+++ tests/Unit/PortfolioManagement/PortfolioItemFactoryTest.php
@@ -39,4 +39,48 @@ class PortfolioItemFactoryTest extends TestCase
             self::assertEquals($tags, $portfolioItem->tags()->toArray());
         }
     }
+
+    /** @test */
+    public function it_should_create_portfolio_item_from_array()
+    {
+        // Given
+        $title = "Test Title";
+        $description = "Test Description";
+        $websiteUrl = "Test Website Url";
+        $image1Url = "Test Image Url 1";
+        $image2Url = "Test Image Url 2";
+        $tag1 = "Tag Name 1";
+        $tag2 = "Tag Name 2";
+        $mainImageUrl = "Main Image Url";
+
+        $portfolioItemAsArray = [
+            "title" => $title,
+            "description" => $description,
+            "website_url" => $websiteUrl,
+            "main_image_url" => $mainImageUrl,
+            "images" => [
+                0 => [
+                    "url" => $image1Url
+                ],
+                1 => [
+                    "url" => $image2Url
+                ]
+            ],
+            "tags" => [
+                $tag1, $tag2
+            ]
+        ];
+
+        // When
+        $portfolioItem = PortfolioItemFactory::fromArray($portfolioItemAsArray);
+
+        // Then
+        self::assertEquals($title, $portfolioItem->title());
+        self::assertEquals($description, $portfolioItem->description());
+        self::assertEquals($websiteUrl, $portfolioItem->websiteUrl());
+        self::assertEquals($image1Url, $portfolioItem->images()->first()->url());
+        self::assertEquals($image2Url, $portfolioItem->images()[1]->url());
+        self::assertEquals($tag1, $portfolioItem->tags()->first());
+        self::assertEquals($tag2, $portfolioItem->tags()[1]);
+    }
 }
