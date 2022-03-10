f7d5f75ae1f521063d157581e023659fa7fab92a
fmeccanici <florismeccanici@mecctech-solutions.nl>
2022-03-10 15:14:14 +0100

    test: add dummy customer repo

---
+++ tests/Feature/CustomerRelationshipManagement/DummyCustomerRepository.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Tests\Feature\CustomerRelationshipManagement;
+
+class DummyCustomerRepository implements \App\CustomerRelationshipManagement\Domain\Repositories\CustomerRepositoryInterface
+{
+
+}
