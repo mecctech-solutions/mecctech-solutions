3b76fb579d5a05f361790f1eb3bc4e38b91acc30
fmeccanici <florismeccanici@mecctech-solutions.nl>
2022-03-10 18:39:47 +0100

    test: add test for customer factor create method

--- app/CustomerRelationshipManagement/Domain/Customers/CustomerFactory.php
+++ app/CustomerRelationshipManagement/Domain/Customers/CustomerFactory.php
@@ -2,10 +2,28 @@
 
 namespace App\CustomerRelationshipManagement\Domain\Customers;
 
+use Faker\Factory;
+use Illuminate\Support\Collection;
+
 class CustomerFactory
 {
     public static function fromArray(array $customer): Customer
     {
         return new Customer($customer["customer_number"], $customer["first_name"], $customer["last_name"], $customer["email"]);
+    }
+
+    public static function create(int $amount = 1, array $attributes = []): Collection
+    {
+        $faker = Factory::create();
+
+        $result = collect();
+
+        for ($i = 0; $i < $amount; $i++)
+        {
+            $customer = new Customer(uniqid(), $faker->firstName, $faker->lastName, $faker->email);
+            $result->push($customer);
+        }
+
+        return $result;
     }
 }
