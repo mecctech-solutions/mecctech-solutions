36c66a27c6eb23cf659f0b1a1ccf2825a3ce0f8c
fmeccanici <florismeccanici@mecctech-solutions.nl>
2022-03-11 10:42:22 +0100

    feat: push send mail job to queue

--- app/CustomerRelationshipManagement/Infrastructure/Services/EmailNotificationSenderService.php
+++ app/CustomerRelationshipManagement/Infrastructure/Services/EmailNotificationSenderService.php
@@ -4,6 +4,7 @@ namespace App\CustomerRelationshipManagement\Infrastructure\Services;
 
 use App\CustomerRelationshipManagement\Domain\Notifications\Notification;
 use App\CustomerRelationshipManagement\Domain\Notifications\Recipient;
+use App\Jobs\SendMailJob;
 use App\Mail\SubmitContactRequestMail;
 use Illuminate\Support\Facades\Mail;
 
@@ -13,7 +14,9 @@ class EmailNotificationSenderService implements \App\CustomerRelationshipManagem
     {
         $mailable = new SubmitContactRequestMail($notification->message(), $recipient->email());
 
-        Mail::to($recipient->email())->send($mailable);
+        SendMailJob::dispatch(
+            $mailable, $recipient->email()
+        )->onQueue('emails');
 
         return $notification;
     }
---
+++ app/Jobs/SendMailJob.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+
+class SendMailJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private Mailable $mailable;
+    private string $recipientEmailAddress;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Mailable $mailable,
+                                string $recipientEmailAddress)
+    {
+        $this->mailable = $mailable;
+        $this->recipientEmailAddress = $recipientEmailAddress;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Mail::to($this->recipientEmailAddress)->send($this->mailable);
+    }
+}
