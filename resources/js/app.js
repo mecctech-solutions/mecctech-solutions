07a75878e40a5894e8918590d73d45e917191da9
fmeccanici <florismeccanici@mecctech-solutions.nl>
2022-03-17 18:40:32 +0100

    build: move skills above skill in component registration

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,6 +10,7 @@ const app = Vue.createApp({
 });
 
 app.component('view-my-work', require('./components/ViewMyWork.vue').default);
+app.component('skills', require('./components/Skills.vue').default);
 app.component('skill', require('./components/Skill.vue').default);
 app.component('portfolio-items', require('./components/PortfolioItems.vue').default);
 app.component('portfolio-item', require('./components/PortfolioItem.vue').default);
@@ -18,7 +19,6 @@ app.component('contact-form', require('./components/ContactForm.vue').default);
 app.component('section-title', require('./components/SectionTitle.vue').default);
 app.component('mecc-tech-footer', require('./components/MeccTechFooter.vue').default);
 app.component('mecc-tech-header', require('./components/MeccTechHeader.vue').default);
-app.component('skills', require('./components/Skills.vue').default);
 
 // Create a new store instance.
 const store = createStore({
