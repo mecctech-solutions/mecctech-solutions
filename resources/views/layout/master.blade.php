3612e8766735def9601a4ee5d0691318182a3202
fmeccanici <florismeccanici@mecctech-solutions.nl>
2022-03-20 16:39:55 +0100

    fix: master should only yield app

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -27,12 +27,7 @@
 <body>
 
 <div id="app">
-    <mecc-tech-header></mecc-tech-header>
-    @yield('content')
-
-    <footer class="fixed inset-x-0 bottom-0">
-        <mecc-tech-footer></mecc-tech-footer>
-    </footer>
+    @yield('app')
 </div>
 </body>
 </html>
