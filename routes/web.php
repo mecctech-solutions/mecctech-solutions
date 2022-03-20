3b53b920b9f958153ab57ed792e17c7ec66820e2
fmeccanici <florismeccanici@mecctech-solutions.nl>
2022-03-20 16:27:07 +0100

    feat: add partners page for plant and styling

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 })->name('home');
+
+Route::get('/partners', function () {
+    return view('partners');
+});
