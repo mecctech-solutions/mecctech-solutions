79c17c3c8d67d6ccf5ea3c391f583d456cf0fba9
fmeccanici <florismeccanici@mecctech-solutions.nl>
2022-04-21 09:53:59 +0200

    feat: update about me text to include other experiences

--- resources/lang/en/about.php
+++ resources/lang/en/about.php
@@ -3,7 +3,7 @@
 return [
     "more_information" => "More Information",
     "less_information" => "Less Information",
-    "who_am_i_text" => "I'm a full-stack web developer and freelancer specialized in writing e-commerce software in Laravel / PHP and Vue.js. Count on me to write extensible and maintainable web applications for your e-commerce business, thus increasing your revenue.",
+    "who_am_i_text" => "I'm a full-stack web developer and freelancer specialized in writing e-commerce software in Laravel / PHP and Vue.js. Count on me to write extensible and maintainable web applications for your e-commerce business, thus increasing your revenue. Although my specialization, I have over 5+ years in writing software in other languages and fields. Either commercial, in my BSc. and MSc. study in Robotics at the TU Delft and as a hobby.",
     "requirements" => "Requirements",
     "requirements_summary" => "I make sure it is clear what you want me to build.",
     "design" => "Design",
--- resources/lang/nl/about.php
+++ resources/lang/nl/about.php
@@ -3,7 +3,7 @@
 return [
     "more_information" => "Meer Informatie",
     "less_information" => "Minder Informatie",
-    "who_am_i_text" => "Ik ben een full stack web developer en freelancer, gespecialiseerd in e-commerce software met Laravel en Vue.js. Ik heb een passie voor het maken van schaalbare en onderhoudbare e-commerce applicaties, waarmee ik bedrijven help hun bedrijfsprocessen te automatiseren.",
+    "who_am_i_text" => "Ik ben een full stack web developer en freelancer, gespecialiseerd in e-commerce software met Laravel en Vue.js. Ik heb een passie voor het maken van schaalbare en onderhoudbare e-commerce applicaties, waarmee ik bedrijven help hun bedrijfsprocessen te automatiseren. Hoewel ik mij nu specialiseer, heb ik meer dan 5 jaar ervaring in het schrijven van software in verschillende talen en werkvelden. Zowel commercieel, als in mijn BSc. / MSc. opleiding in Robotica aan de TU Delft, en als hobby.",
     "requirements" => "Requirements",
     "requirements_summary" => "Ik zorg ervoor dat het duidelijk wordt wat er gemaakt moet worden",
     "design" => "Ontwerp",
