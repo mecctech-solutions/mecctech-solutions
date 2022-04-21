939bdc017f846a85cf0ec7261ec2ff9a6beca7d7
fmeccanici <florismeccanici@mecctech-solutions.nl>
2022-04-21 11:12:49 +0200

    feat: add tag navbar

--- resources/js/components/PortfolioItems.vue
+++ resources/js/components/PortfolioItems.vue
@@ -3,6 +3,18 @@
         <h1 class="text-4xl font-bold mt-10">{{ $lang.get('sections.projects') }}</h1>
         <div class="border-t border-4 border-black mt-5 w-1/16"></div>
 
+        <div>
+            <ul class="flex m-10 text-xl">
+                <li :class="{'bg-mecctech-red text-white' : this.isSelected('All')}" @click="this.selectTag('All')" class="pl-10 pr-10 pt-3 pb-3 cursor-pointer" id="tag-all">{{ $lang.get('tags.all') }}</li>
+                <li :class="{'bg-mecctech-red text-white' : this.isSelected('Laravel')}" @click="this.selectTag('Laravel')" class="pl-10 pr-10 pt-3 pb-3 cursor-pointer" id="tag-laravel">Laravel</li>
+                <li :class="{'bg-mecctech-red text-white' : this.isSelected('Vue.js')}" @click="this.selectTag('Vue.js')" class="pl-10 pr-10 pt-3 pb-3 cursor-pointer" id="tag-vue">Vue.js</li>
+                <li :class="{'bg-mecctech-red text-white' : this.isSelected('Python')}" @click="this.selectTag('Python')" class="pl-10 pr-10 pt-3 pb-3 cursor-pointer" id="tag-python">Python</li>
+                <li :class="{'bg-mecctech-red text-white' : this.isSelected('C++')}" @click="this.selectTag('C++')" class="pl-10 pr-10 pt-3 pb-3 cursor-pointer" id="tag-c++">C++</li>
+                <li :class="{'bg-mecctech-red text-white' : this.isSelected('C#')}" @click="this.selectTag('C#')" class="pl-10 pr-10 pt-3 pb-3 cursor-pointer" id="tag-c#">C#</li>
+                <li :class="{'bg-mecctech-red text-white' : this.isSelected('C')}" @click="this.selectTag('C')" class="pl-10 pr-10 pt-3 pb-3 cursor-pointer" id="tag-arduino">C</li>
+                <li :class="{'bg-mecctech-red text-white' : this.isSelected('Matlab')}" @click="this.selectTag('Matlab')" class="pl-10 pr-10 pt-3 pb-3 cursor-pointer" id="tag-matlab">Matlab</li>
+            </ul>
+        </div>
         <div class="grid grid-cols-1 md:grid-cols-3 pl-16 pr-16 lg:pl-96 lg:pr-96 mt-20">
             <portfolio-item v-for="portfolio_item in this.portfolio_items" :title="locale === 'nl' ? portfolio_item.title.dutch : portfolio_item.title.english" :description="locale === 'nl' ? portfolio_item.description.dutch : portfolio_item.description.english" :tags="portfolio_item.tags" :main_image_url="portfolio_item.main_image.url" :website_url="portfolio_item.website_url" :images="portfolio_item.images"></portfolio-item>
         </div>
@@ -20,7 +32,8 @@
         data() {
             return {
                 portfolio_items: [],
-                current_page : 1
+                current_page : 1,
+                selected_tag : 'All'
             }
         },
         mounted() {
@@ -40,6 +53,15 @@
                     .then(( { data } ) => {
                         this.portfolio_items = data.payload.portfolio_items.data;
                     });
+            },
+            isSelected(tagName)
+            {
+                return this.selected_tag === tagName;
+            },
+            selectTag(tagName)
+            {
+                this.selected_tag = tagName;
+                this.getPortfolioItemsWithTag(tagName);
             }
         }
 
