9f763f3f000c5fc477baececac44e3d6703d4918
fmeccanici <florismeccanici@mecctech-solutions.nl>
2022-09-30 11:17:24 +0200

    Add skills and my approach section

---
+++ resources/js/components/SkillV2.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="ud-mb-6">
+        <div class="ud-flex ud-justify-between ud-mb-1">
+            <span class="ud-text-lg ud-text-black ud-font-bold">{{ this.name }}</span>
+            <span class="ud-text-base ud-text-black ud-font-bold">{{ this.level }}</span>
+        </div>
+        <div class="ud-w-full ud-bg-gray-200 ud-rounded-full ud-h-2.5 dark:ud-bg-gray-700">
+            <div class="ud-bg-primary ud-h-2.5 ud-rounded-full" :style="{ 'width' : this.percentage + '%'}"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['name', 'level', 'percentage'],
+    name: "SkillsV2"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/SkillsV2.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="ud-w-1/2 ud-mx-auto">
+        <skill-v2 :name="'Laravel'" :level="'Advanced'" :percentage="90"></skill-v2>
+        <skill-v2 :name="'PHP'" :level="'Advanced'" :percentage="90"></skill-v2>
+        <skill-v2 :name="'Vue.js'" :level="'Intermediate'" :percentage="80"></skill-v2>
+        <skill-v2 :name="'TailwindCSS'" :level="'Intermediate'" :percentage="80"></skill-v2>
+        <skill-v2 :name="'HTML'" :level="'Intermediate'" :percentage="80"></skill-v2>
+        <skill-v2 :name="'Javascript'" :level="'Intermediate'" :percentage="70"></skill-v2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SkillsV2"
+}
+
+</script>
+
+<style scoped>
+
+</style>
