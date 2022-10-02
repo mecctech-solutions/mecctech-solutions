6f167d02cfde64216a8b189e3a2ca269bec27911
fmeccanici <florismeccanici@mecctech-solutions.nl>
2022-10-02 19:26:35 +0200

    refactor: zzp

--- resources/lang/nl/home.php
+++ resources/lang/nl/home.php
@@ -3,7 +3,7 @@
 return [
     "e_commerce" => "E-commerce web applicaties.",
     "hi" => "Hi! Ik ben",
-    "freelance" => "ZZPer",
+    "freelance" => "ZZP",
     "i_am" => "Laravel en Vue.js ontwikkelaar",
     "headline" => "Reken op mij voor het schrijven van complete web applicaties die schaalbaar en onderhoudbaar zijn.",
     "contact" => "Contact",
