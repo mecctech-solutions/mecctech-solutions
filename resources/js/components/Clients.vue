1ad903243f6e210ff2343ec04054e788498ccdc3
fmeccanici <florismeccanici@mecctech-solutions.nl>
2022-10-02 12:58:55 +0200

    feat: remove rybers

--- resources/js/components/Clients.vue
+++ resources/js/components/Clients.vue
@@ -99,28 +99,6 @@
                         >
                             <img src="/images/clients/supra_solutions_logo.png" alt="supra_solutions" />
                         </a>
-                        <a
-                            href="https://www.ryberg.nl/"
-                            target="_blank"
-                            rel="nofollow noreferrer"
-                            class="
-                  ud-flex ud-items-center ud-justify-center ud-max-w-[120px]
-                  lg:ud-max-w-[130px]
-                  xl:ud-max-w-[150px]
-                  2xl:ud-max-w-[160px]
-                  ud-mx-3
-                  sm:ud-mx-4
-                  xl:ud-mx-6
-                  2xl:ud-mx-8
-                  ud-py-[15px] ud-grayscale
-                  hover:ud-grayscale-0
-                  ud-opacity-60
-                  hover:ud-opacity-100
-                  ud-transition
-                "
-                        >
-                            <img src="/images/clients/ryberg_logo.png" alt="ryberg" />
-                        </a>
                     </div>
                 </div>
             </div>
