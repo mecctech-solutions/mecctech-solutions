e1f8f8a8bea8d090ae858624641eb07ebd309352
fmeccanici <florismeccanici@mecctech-solutions.nl>
2022-10-06 12:15:18 +0200

    Change connect text

--- resources/lang/nl/about.php
+++ resources/lang/nl/about.php
@@ -3,7 +3,7 @@
 return [
     "about_me" => "OVER MIJ",
     "about_me_text" => "Ik heb een passie voor het maken van hoge kwaliteit Laravel applicaties die schaalbaar / onderhoudbaar zijn, die complexe business problemen oplossen en fijn zijn voor de gebruiker, dus hun leven te verbeteren.",
-    "connect" => "Connect Met Mij",
+    "connect" => "Get In Touch",
     "connect_text" => "Als je een interessant idee of project hebt wat je wil laten ontwikkelen met Laravel en Vue.js, voel je vrij om mij te benaderen via het contact formulier, via social media of via mijn persoonlijke contact informatie.",
     "solving_problems" => "Problemen oplossen met hoge kwaliteit software",
 ];
