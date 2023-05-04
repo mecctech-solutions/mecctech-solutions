483f9543b44d2817f5a921b3f946996f6d7eb58b
fmeccanici <florismeccanici@mecctech-solutions.nl>
2023-05-04 11:41:05 +0200

    Add google tag

--- resources/views/components/master.blade.php
+++ resources/views/components/master.blade.php
@@ -33,6 +33,15 @@
     <script defer src="{{ mix('js/app.js') }}"></script>
     <script src="/lang-{{ app()->getLocale() }}.js"></script>
 
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-VB61P7MT21"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-VB61P7MT21');
+    </script>
 
 </head>
     <body>
