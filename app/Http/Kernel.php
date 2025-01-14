5f0bc950f30a20858e0d91a4810d1c4e44946d67
fmeccanici <florismeccanici@mecctech-solutions.nl>
2025-01-13 20:22:44 -0800

    Add correct CORS middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,8 +2,8 @@
 
 namespace App\Http;
 
-use App\Http\Middleware\HandleCorsMiddleware;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
+use Illuminate\Http\Middleware\HandleCors;
 
 class Kernel extends HttpKernel
 {
@@ -21,7 +21,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        HandleCorsMiddleware::class
+        HandleCors::class
     ];
 
     /**
@@ -64,6 +64,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'cors' => HandleCorsMiddleware::class
+        'cors' => HandleCors::class
     ];
 }
