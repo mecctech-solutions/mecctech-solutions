d9680b000c1e2cd61a598db34ca6f853b4d64dcf
fmeccanici <florismeccanici@mecctech-solutions.nl>
2025-01-14 12:17:27 -0800

    Return correct url for image

--- app/PortfolioManagement/Infrastructure/Persistence/Eloquent/PortfolioItems/EloquentImage.php
+++ app/PortfolioManagement/Infrastructure/Persistence/Eloquent/PortfolioItems/EloquentImage.php
@@ -2,16 +2,25 @@
 
 namespace App\PortfolioManagement\Infrastructure\Persistence\Eloquent\PortfolioItems;
 
-/**
- * @property string $title
- * @property string $main_image_url;
- * @property string $description;
- * @property string $website_url
- */
+use Illuminate\Database\Eloquent\Casts\Attribute;
+
 class EloquentImage extends \Illuminate\Database\Eloquent\Model
 {
     protected $table = "images";
     protected $guarded = [];
 
+    public function url(): Attribute
+    {
+        return new Attribute(
+            get: function ($value) {
+                if (\Storage::exists($value)) {
+                    return \Storage::url($value);
+                }
 
+                return $value;
+            },
+            set: function ($value) {
+                return $value;
+            });
+    }
 }
---
+++ tests/Unit/PortfolioManagement/EloquentImageTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests\Unit\PortfolioManagement;
+
+use App\PortfolioManagement\Infrastructure\Persistence\Eloquent\PortfolioItems\EloquentImage;
+use App\PortfolioManagement\Infrastructure\Persistence\Eloquent\PortfolioItems\EloquentPortfolioItem;
+use Tests\TestCase;
+
+class EloquentImageTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->eloquentPortfolioItem = new EloquentPortfolioItem([
+            "title_en" => "Test Title",
+            "title_nl" => "Test Titel",
+            "main_image_url" => "Test Url",
+            "description_nl" => "Test Beschrijving",
+            "description_en" => "Test Description",
+            "website_url" => "Test Website Url"
+        ]);
+
+        $this->eloquentPortfolioItem->save();
+
+        $this->eloquentImage = new EloquentImage([
+            "url" => "/images/placeholder.png",
+            "portfolio_item_id" => $this->eloquentPortfolioItem->id,
+        ]);
+    }
+
+    /** @test */
+    public function it_should_return_main_image_url_with_storage_when_it_exists()
+    {
+        // Arrange
+        \Storage::fake();
+        $fileName = "test.jpg";
+        \Storage::put($fileName, "test");
+        $this->eloquentImage->url = $fileName;
+        $this->eloquentImage->save();
+
+        // Act & Assert
+        self::assertEquals("/storage/test.jpg", $this->eloquentImage->url);
+    }
+
+    /** @test */
+    public function it_should_return_main_image_url_without_storage_when_it_does_not_exist()
+    {
+        // Arrange
+        $this->eloquentImage->url = "test.jpg";
+        $this->eloquentImage->save();
+
+        // Act & Assert
+        self::assertEquals("test.jpg", $this->eloquentImage->url);
+    }
+}
+
