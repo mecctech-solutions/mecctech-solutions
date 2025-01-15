a6c32d9333f8442baacd237c5f18bd260109f764
fmeccanici <florismeccanici@mecctech-solutions.nl>
2025-01-15 15:06:22 -0800

    Working without translations

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import {defineConfig} from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+import Components from 'unplugin-vue-components/vite';
+
+export default defineConfig({
+    plugins: [
+        laravel([
+            'resources/css/app.css',
+            'resources/js/app.js',
+        ]),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+        Components({
+            dirs: ['resources/js/Components'],
+            extensions: ['vue'],
+            deep: true,
+        }),
+    ],
+});
