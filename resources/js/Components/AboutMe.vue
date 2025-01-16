2e48fddd65068935ba09bfcc6abfc34248cbdd47
fmeccanici <florismeccanici@mecctech-solutions.nl>
2025-01-16 13:38:32 -0800

    Move to SocialLink.vue component

--- resources/js/Components/AboutMe.vue
+++ resources/js/Components/AboutMe.vue
@@ -80,37 +80,14 @@
                                 {{ trans('home.contact') }}
                             </a>
                             <div class="ud-flex ud-items-center">
-                                <a
-                                    href="https://www.linkedin.com/in/florismeccanici"
-                                    class="
-                    ud-w-9
-                    ud-h-9
-                    ud-mr-3
-                    ud-rounded-full
-                    ud-flex
-                    ud-items-center
-                    ud-justify-center
-                    ud-border
-                    ud-border-[#E5E8F2]
-                    ud-text-body-color
-                    hover:ud-bg-primary
-                    hover:ud-border-primary
-                    hover:ud-text-white
-                  "
-                                    aria-label="social-link"
-                                    name="social-link"
-                                >
-                                    <svg
-                                        width="14"
-                                        height="14"
-                                        viewBox="0 0 14 14"
-                                        class="ud-fill-current"
-                                    >
-                                        <path
-                                            d="M13.0214 0H1.02084C0.453707 0 0 0.451613 0 1.01613V12.9839C0 13.5258 0.453707 14 1.02084 14H12.976C13.5432 14 13.9969 13.5484 13.9969 12.9839V0.993548C14.0422 0.451613 13.5885 0 13.0214 0ZM4.15142 11.9H2.08705V5.23871H4.15142V11.9ZM3.10789 4.3129C2.42733 4.3129 1.90557 3.77097 1.90557 3.11613C1.90557 2.46129 2.45002 1.91935 3.10789 1.91935C3.76577 1.91935 4.31022 2.46129 4.31022 3.11613C4.31022 3.77097 3.81114 4.3129 3.10789 4.3129ZM11.9779 11.9H9.9135V8.67097C9.9135 7.90323 9.89082 6.8871 8.82461 6.8871C7.73571 6.8871 7.57691 7.74516 7.57691 8.60322V11.9H5.51254V5.23871H7.53154V6.16452H7.55423C7.84914 5.62258 8.50701 5.08065 9.52785 5.08065C11.6376 5.08065 12.0232 6.43548 12.0232 8.2871V11.9H11.9779Z"
-                                        />
-                                    </svg>
-                                </a>
+                                <SocialLink
+                                    url="https://www.linkedin.com/in/florismeccanici/"
+                                    type="linkedin"
+                                />
+                                <SocialLink
+                                    url="https://github.com/mecctech-solutions"
+                                    type="github"
+                                />
                             </div>
                         </div>
                         <div>
@@ -167,6 +144,7 @@
 import {computed} from "vue";
 import {usePage} from "@inertiajs/vue3";
 import {trans} from "laravel-vue-i18n";
+import SocialLink from "@/Components/SocialLink.vue";
 
 const page = usePage();
 const locale = computed(() => page.props.locale);
---
+++ resources/js/Components/SocialLink.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import {defineProps} from 'vue';
+
+const props = defineProps({
+    url: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: String,
+        required: true,
+        validator: (value: string) => {
+            return ['twitter', 'linkedin', 'facebook', 'instagram', 'github'].includes(value);
+        },
+    },
+});
+</script>
+
+<template>
+    <a
+        :href="url"
+        class="
+                    ud-w-9
+                    ud-h-9
+                    ud-mr-3
+                    ud-rounded-full
+                    ud-flex
+                    ud-items-center
+                    ud-justify-center
+                    ud-border
+                    ud-border-[#E5E8F2]
+                    ud-text-body-color
+                    hover:ud-bg-primary
+                    hover:ud-border-primary
+                    hover:ud-text-white
+                  "
+        aria-label="social-link"
+        name="social-link"
+    >
+        <i
+            :class="[
+                        'fa-brands',
+                        {
+                            'fa-twitter': type === 'twitter',
+                            'fa-linkedin': type === 'linkedin',
+                            'fa-facebook': type === 'facebook',
+                            'fa-instagram': type === 'instagram',
+                            'fa-github': type === 'github',
+                        },
+                    ]"
+        ></i>
+    </a>
+</template>
+
+<style scoped>
+
+</style>
