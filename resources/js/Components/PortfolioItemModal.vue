647277744fd325084fec250f89fdb485d89c7c8b
fmeccanici <florismeccanici@mecctech-solutions.nl>
2025-01-15 17:16:03 -0800

    Image carousel working for mobile and desktop

--- resources/js/Components/PortfolioItemModal.vue
+++ resources/js/Components/PortfolioItemModal.vue
@@ -4,21 +4,10 @@
       class="ud-bg-white ud-z-10 centered ud-pb-20 ud-mt-10"
       v-click-outside="emitTurnOffModalEvent"
   >
-    <div class="ud-flex ud-justify-around ud-items-center">
-      <i
-          @click="previousImage"
-          class="fa-solid fa-angle-left ud-text-xl md:ud-text-5xl ud-pt-5 ud-pb-5 ud-pr-10 ud-pl-10 ud-text-primary hover:ud-scale-125 ud-transform ud-transition ud-ease-in-out ud-duration-500 ud-cursor-pointer"
-      ></i>
-      <img
-          class="uw-w-32 md:ud-w-200 md:ud-h-100 lg:ud-h-140 ud-object-scale-down"
-          :src="currentImageUrl"
-          alt=""
-      />
-      <i
-          @click="nextImage"
-          class="fa-solid fa-angle-right ud-text-xl md:ud-text-5xl ud-p-5 ud-pr-10 ud-pl-10 ud-text-primary hover:ud-scale-125 ud-transform ud-transition ud-ease-in-out ud-duration-500 ud-cursor-pointer"
-      ></i>
-    </div>
+    <ImageCarousel
+        :images="images"
+        @updateCurrentImage="currentImageUrl = $event"
+    ></ImageCarousel>
     <div class="ud-text-left ud-p-5">
       <div class="ud-flex ud-items-center ud-space-x-3">
         <h1 class="ud-text-4xl ud-font-bold">{{ title }}</h1>
@@ -60,25 +49,10 @@
       v-else
       class="ud-fixed ud-z-50 ud-top-0 ud-h-full ud-m-0 ud-bg-white ud-left-0"
   >
-    <div class="ud-flex ud-flex-col ud-items-center">
-      <img
-          class="ud-w-200 ud-h-60 ud-object-scale-down"
-          :src="currentImageUrl"
-          alt=""
-      />
-      <div class="ud-flex ud-space-x-2 ud-mt-10">
-        <i
-            v-for="image in images"
-            :key="image.url"
-            @click="currentImageUrl = image.url"
-            :class="{
-            'ud-text-primary': image.url === currentImageUrl,
-            'ud-text-black': image.url !== currentImageUrl,
-          }"
-            class="fas fa-circle ud-cursor-pointer"
-        ></i>
-      </div>
-    </div>
+    <ImageCarousel
+        :images="images"
+        @updateCurrentImage="currentImageUrl = $event">
+    </ImageCarousel>
     <div class="ud-p-5 ud-mb-5">
       <div class="ud-mb-4">
         <h1 class="ud-text-2xl ud-font-bold">{{ title }}</h1>
@@ -127,6 +101,7 @@
 <script setup>
 import {computed, onMounted, ref} from "vue";
 import {usePage} from "@inertiajs/vue3";
+import ImageCarousel from "./ImageCarousel.vue";
 
 const props = defineProps({
   title: String,
@@ -142,37 +117,23 @@ const emit = defineEmits(["turn-off-modal"]);
 const page = usePage();
 const locale = computed(() => page.props.locale);
 
-const currentImageUrl = ref("");
-const currentImageIndex = ref(0);
-const maxImageIndex = computed(() => props.images.length);
 const isMobile = ref(window.innerWidth <= 760);
+const currentImageUrl = ref("");
 
 const updateMobileStatus = () => {
-  isMobile.value = window.innerWidth <= 760;
+    isMobile.value = window.innerWidth <= 760;
 };
 
-const nextImage = () => {
-  if (currentImageIndex.value < maxImageIndex.value - 1) {
-    currentImageIndex.value++;
-    currentImageUrl.value = props.images[currentImageIndex.value].url;
-  }
-};
-
-const previousImage = () => {
-  if (currentImageIndex.value > 0) {
-    currentImageIndex.value--;
-    currentImageUrl.value = props.images[currentImageIndex.value].url;
-  }
-};
+onMounted(() => {
+    currentImageUrl.value = props.images[0].url;
+    window.addEventListener("resize", updateMobileStatus);
+});
 
 const emitTurnOffModalEvent = () => {
   emit("turn-off-modal");
 };
 
-onMounted(() => {
-  currentImageUrl.value = props.images[0].url;
-  window.addEventListener("resize", updateMobileStatus);
-});
+
 </script>
 
 <style scoped>
--- vite.config.js
+++ vite.config.js
@@ -24,5 +24,6 @@ export default defineConfig({
             extensions: ['vue'],
             deep: true,
         }),
+
     ],
 });
