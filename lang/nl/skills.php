37e4ea81f5c849694f517ddf4b501d10b96af379
fmeccanici <florismeccanici@mecctech-solutions.nl>
2025-01-16 16:30:05 -0800

    Improve skill section

--- lang/nl/skills.php
+++ lang/nl/skills.php
@@ -3,6 +3,64 @@
 return [
     "offer" => "Wat heb ik te bieden?",
     "my_skills" => "Mijn Skills",
-    "advanced" => "Geavanceerd",
-    "intermediate" => "Gevorderd",
+    'Laravel' => [
+        'name' => 'Laravel',
+        'description' => 'Framework voor backend-ontwikkeling',
+    ],
+    'Vue.js' => [
+        'name' => 'Vue.js',
+        'description' => 'Frontend JavaScript-framework',
+    ],
+    'Inertia.js' => [
+        'name' => 'Inertia.js',
+        'description' => 'Brug tussen Laravel en Vue.js voor SPAs',
+    ],
+    'PHP' => [
+        'name' => 'PHP',
+        'description' => 'Kernprogrammeertaal voor Laravel-ontwikkeling',
+    ],
+    'Javascript' => [
+        'name' => 'Javascript',
+        'description' => 'Kernprogrammeertaal voor Vue.js-ontwikkeling',
+    ],
+    'MySQL' => [
+        'name' => 'MySQL',
+        'description' => 'Relationeel databasesysteem',
+    ],
+    'TailwindCSS' => [
+        'name' => 'TailwindCSS',
+        'description' => 'Utility-first CSS-framework',
+    ],
+    'Laravel Sail & Docker' => [
+        'name' => 'Laravel Sail & Docker',
+        'description' => 'Platform voor containerisatie',
+    ],
+    'Redis' => [
+        'name' => 'Redis',
+        'description' => 'Caching en berichtenservice',
+    ],
+    'Pest & PHP Unit' => [
+        'name' => 'Pest & PHP Unit',
+        'description' => 'Testframeworks voor PHP',
+    ],
+    'Laravel Forge' => [
+        'name' => 'Laravel Forge',
+        'description' => 'Serverbeheer en implementatie',
+    ],
+    'CI/CD (GitHub Actions)' => [
+        'name' => 'CI/CD (GitHub Actions)',
+        'description' => 'Geautomatiseerde pijplijnen voor implementatie',
+    ],
+    'Laravel Dusk' => [
+        'name' => 'Laravel Dusk',
+        'description' => 'Tool voor browser-tests',
+    ],
+    'Webpack & Vite' => [
+        'name' => 'Webpack & Vite',
+        'description' => 'Frontend-buildtools',
+    ],
+    'Git' => [
+        'name' => 'Git',
+        'description' => 'Versiebeheersysteem',
+    ],
 ];
