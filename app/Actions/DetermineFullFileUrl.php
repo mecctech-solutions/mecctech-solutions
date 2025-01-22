034d72de464d954eb8ed0637d99953fefe6dbcde
fmeccanici <florismeccanici@mecctech-solutions.nl>
2025-01-22 10:26:03 -0800

    Add comments

--- app/Actions/DetermineFullFileUrl.php
+++ app/Actions/DetermineFullFileUrl.php
@@ -11,6 +11,8 @@ class DetermineFullFileUrl
 
     public function handle(string $filePath): string
     {
+        // Needs to be hard coded with public to make the tests pass
+        // Likely an environment caching issue but solved it like this for now
         if (Storage::disk('public')->exists($filePath)) {
             return Storage::disk('public')->url($filePath);
         }
