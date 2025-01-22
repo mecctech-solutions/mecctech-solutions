c6bf6913f539f2ef86d18b34ec20a351ab966399
fmeccanici <florismeccanici@mecctech-solutions.nl>
2025-01-21 16:35:04 -0800

    Make descriptions nullable

---
+++ database/migrations/2025_01_22_003349_add_nullable_description_to_portfolio_items_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('portfolio_items', function (Blueprint $table) {
+            $table->text('description_nl')->nullable()->change();
+            $table->text('description_en')->nullable()->change();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('portfolio_items', function (Blueprint $table) {
+            $table->text('description_nl')->nullable(false)->change();
+            $table->text('description_en')->nullable(false)->change();
+        });
+    }
+};
