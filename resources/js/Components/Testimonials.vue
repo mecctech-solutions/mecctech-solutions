5efd171fe203361ba63e19a17dd3ffdffc8c3483
fmeccanici <florismeccanici@mecctech-solutions.nl>
2025-01-23 13:01:18 -0800

    Add type

--- resources/js/Components/Testimonials.vue
+++ resources/js/Components/Testimonials.vue
@@ -48,12 +48,12 @@ import {usePage} from '@inertiajs/vue3';
 import {computed} from 'vue';
 import {trans} from "laravel-vue-i18n";
 
-interface PageProps {
+interface Props {
     testimonials: Array<App.Data.TestimonialData>;
     locale: string;
 }
 
-const page = usePage<PageProps>();
+const page = usePage<Props>();
 
 const testimonials = computed(() => page.props.testimonials);
 const locale = computed(() => page.props.locale);
