66ddfa08b23b73fee325d8f159f8124f76dfcb76
fmeccanici <florismeccanici@mecctech-solutions.nl>
2025-06-09 17:58:19 +0200

    Update content

--- lang/nl/home.php
+++ lang/nl/home.php
@@ -5,7 +5,7 @@ return [
     'hi' => 'Hoi! Ik ben',
     'freelance' => 'ZZP',
     'i_am' => 'Laravel en Vue.js ontwikkelaar',
-    'headline' => 'Op maat gemaakte en robuuste web applicaties om jouw ideeën te realiseren.',
+    'headline' => 'Op maat gemaakte web applicaties om jouw ideeën te realiseren.',
     'contact' => 'Contact',
-    'learn_more' => 'Meer Informatie',
+    'learn_more' => 'Bekijk mijn werk',
 ];
