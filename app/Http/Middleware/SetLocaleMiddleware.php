770a5b5ad77de1df38e3b02f5f3d71b9e3d6ce6c
fmeccanici <florismeccanici@mecctech-solutions.nl>
2025-06-09 17:32:45 +0200

    Make dusk test pass

--- app/Http/Middleware/SetLocaleMiddleware.php
+++ app/Http/Middleware/SetLocaleMiddleware.php
@@ -3,12 +3,14 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class SetLocaleMiddleware
 {
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next): Response|RedirectResponse|JsonResponse
     {
         if (session()->has('locale')) {
             app()->setLocale(session('locale'));
