e75b4e0bf378f053ae351bdca2d71e650ca6bd78
fmeccanici <florismeccanici@mecctech-solutions.nl>
2025-06-09 08:28:47 +0200

    Add extra test

--- tests/Feature/Controllers/SubmitContactRequestTest.php
+++ tests/Feature/Controllers/SubmitContactRequestTest.php
@@ -9,7 +9,6 @@ class SubmitContactRequestTest extends TestCase
     /** @test */
     public function it_should_return_status_redirect()
     {
-
         // Given
         $url = route('submit-contact-request');
 
@@ -27,5 +26,25 @@ class SubmitContactRequestTest extends TestCase
 
         // Then
         self::assertEquals(302, $response->status());
+    }
+
+    /** @test */
+    public function it_should_store_contact_request()
+    {
+        // Given
+        $url = route('submit-contact-request');
+        $contactRequestData = [
+            'first_name' => 'John',
+            'last_name' => 'Doe',
+            'email' => 'john@example.com',
+            'message' => 'Test Message',
+            'phone' => '0612345678',
+        ];
+
+        // When
+        $this->post($url, $contactRequestData);
+
+        // Then
+        $this->assertDatabaseHas('contact_requests', $contactRequestData);
     }
 }
