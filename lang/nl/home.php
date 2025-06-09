fa90143b5bfa67cde4b16420550f073666cb29cd
fmeccanici <florismeccanici@mecctech-solutions.nl>
2025-06-09 17:44:17 +0200

    Set standard language to dutch

--- lang/nl/home.php
+++ lang/nl/home.php
@@ -2,10 +2,10 @@
 
 return [
     'e_commerce' => 'E-commerce web applicaties.',
-    'hi' => 'Hi! Ik ben',
+    'hi' => 'Hoi! Ik ben',
     'freelance' => 'ZZP',
     'i_am' => 'Laravel en Vue.js ontwikkelaar',
-    'headline' => 'Op maat gemaakte en robuuste web applicaties om jouw bedrijfsprocessen te optimaliseren',
+    'headline' => 'Op maat gemaakte en robuuste web applicaties om jouw ideeën te realiseren.',
     'contact' => 'Contact',
     'learn_more' => 'Meer Informatie',
 ];
