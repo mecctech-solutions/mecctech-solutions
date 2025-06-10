9e4bf51e58f9c132ccb19d7a49c0bc81d5c76ddb
fmeccanici <florismeccanici@mecctech-solutions.nl>
2025-06-10 18:37:59 +0200

    Change content

--- lang/nl/about.php
+++ lang/nl/about.php
@@ -4,6 +4,6 @@ return [
     'about_me' => 'Over mij',
     'about_me_text' => 'Ik transformeer jouw ideeën tot robuuste web applicaties. Met mijn expertise in Laravel en Vue.js creëer ik schaalbare, onderhoudbare applicaties om jouw ideeën te realiseren.',
     'connect' => 'Laten we samenwerken',
-    'connect_text' => 'Heb je een innovatief idee of droom die je wilt verwezenlijken? Ik help je graag om jouw visie om te zetten in een hoogwaardige digitale oplossing. Neem contact op via het formulier of mijn persoonlijke contactgegevens.',
+    'connect_text' => 'Heb je een innovatief idee of droom die je wilt verwezenlijken? Ik help je graag om jouw visie om te zetten in een hoogwaardige web applicatie. Neem contact op via het formulier of mijn persoonlijke contactgegevens.',
     'solving_problems' => 'Van idee naar werkelijkheid: jouw dromen, mijn expertise',
 ];
