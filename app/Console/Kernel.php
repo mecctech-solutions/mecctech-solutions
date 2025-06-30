59ad724739492cf0a0582b6a999bd4c743e4059c
fmeccanici <florismeccanici@mecctech-solutions.nl>
2025-06-30 16:53:57 +0200

    Add weekly schedule for backup cleanup command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('backup:run')->weekly();
+        $schedule->command('backup:clean')->weekly();
     }
 
     /**
