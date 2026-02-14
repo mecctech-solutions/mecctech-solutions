b82f240650db1374e393b8454984998928316958
fmeccanici <florismeccanici@mecctech-solutions.nl>
2026-02-14 14:50:53 +0100

    Remove private storage path from backup configuration

--- config/backup.php
+++ config/backup.php
@@ -17,7 +17,6 @@ return [
                 'include' => [
                     base_path(),
                     storage_path('app/public'),
-                    storage_path('app/private'),
                 ],
 
                 /*
