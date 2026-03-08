583475f35ff76f96169dc2251310714c75859f61
fmeccanici <florismeccanici@mecctech-solutions.nl>
2026-03-08 12:23:49 +0100

    Update Dutch translation for about section in lang file

--- lang/nl/about.php
+++ lang/nl/about.php
@@ -4,6 +4,6 @@ return [
     'about_me' => 'Over mij',
     'about_me_text' => "Ik vertaal ideeën naar robuuste webapplicaties. \n Met Laravel en Vue.js bouw ik schaalbare en onderhoudbare software die klaar is voor groei. Geen quick fixes, maar een solide technische basis.",
     'connect' => 'Samenwerken?',
-    'connect_text' => "Heb je een idee voor een webapplicatie en zoek je iemand die het kan uitwerken en bouwen? \n Ik denk graag met je mee en help je van eerste concept tot een productieklare applicatie.",
+    'connect_text' => "Heb je een idee voor een webapplicatie? \n Ik help je van concept tot een werkende applicatie. \n \n Werk je al met Laravel? \n Ik help teams met het bouwen en uitbreiden van Laravel en Vue.js applicaties.",
     'solving_problems' => 'Van idee naar werkelijkheid: jouw product, mijn expertise',
 ];
